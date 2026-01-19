556d6476fd1d720fbd9e95cf770d51e970a77cf7
rathodswapnil972-droid <rathodswapnil972@gmail.com>
2026-01-19 22:26:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -392,5 +392,8 @@ async def analyze(file: UploadFile = File(...)):
 # ================= RUN =================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
